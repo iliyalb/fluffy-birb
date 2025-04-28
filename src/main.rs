463a1fa07fcfdfa294aa2569bbda4c9d9ee7a465
ILIYA LESANI <iliyalesani@gmail.com>
2025-04-28 06:31:40 +0330

    display window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{widget::{column, text}, Element, Settings, application};
+
+#[derive(Debug, Clone)]
+enum Message {}
+
+fn update(_state: &mut (), _message: Message) {}
+
+fn view<'a>(_state: &'a ()) -> Element<'a, Message> {
+    column![text("Hello, world!")].into()
+}
+
+fn main() -> iced::Result {
+    application(
+        "Fluffy Birb", // title
+        update,
+        view,
+    )
+    .run()
 }
